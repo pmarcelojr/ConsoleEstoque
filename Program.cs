d20a7f8224d210c2d54d386b52ee046d6b395f76
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-14 17:59:10 -0300

    creating repetition structure

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleEstoque
         static void Main(string[] args)
         {
             string resp;
+            int opcao;
             Produto p1 = new Produto("TV", 900.00, 10);
 
             Console.Clear();
@@ -19,11 +20,30 @@ namespace ConsoleEstoque
             
             do
             {
-                Console.Write("\nGostaria de adicionar quantas quantidade do produto: ");
-                p1.AddProduto(int.Parse(Console.ReadLine()));
-                Console.WriteLine($"Dados Atualizados: {p1}");
+                Console.WriteLine("Escolha uma Operação\n1 - Adicionar Produto\n2- - Remover Produtos\n3 - Sair");
+                opcao = int.Parse(Console.ReadLine());
 
-                Console.Write("Você deseja adicionar mais quantidades (Sim/Não): ");
+                switch(opcao)
+                {
+                    case 1:
+                        Console.Write("\nGostaria de adicionar quantas quantidade do produto: ");
+                        p1.AddProduto(int.Parse(Console.ReadLine()));
+                        Console.WriteLine($"Dados Atualizados: {p1}");
+                        break;
+                    case 2:
+                        Console.Write("\nGostaria de remover quantos produtos: ");
+                        p1.DelProduto(int.Parse(Console.ReadLine()));
+                        Console.WriteLine($"Dados Atualizados: {p1}");
+                        break;
+                    case 3:
+                        resp = "Sim";
+                        break;
+                    default:
+                        Console.WriteLine("Opção invalida");
+                        break;
+                }
+
+                Console.Write("Você deseja fazer outra operação (Sim/Não): ");
                 resp = Console.ReadLine();
             } while (resp == "Sim" || resp == "SIM" || resp == "sim" || resp == "s" || resp == "S");
             
