b3e6524c9344e4bc138f963bac56e015701d21d1
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-14 18:03:45 -0300

    creating exit option

--- Program.cs
+++ Program.cs
@@ -23,28 +23,28 @@ namespace ConsoleEstoque
                 Console.WriteLine("Escolha uma Operação\n1 - Adicionar Produto\n2- - Remover Produtos\n3 - Sair");
                 opcao = int.Parse(Console.ReadLine());
 
-                switch(opcao)
-                {
-                    case 1:
-                        Console.Write("\nGostaria de adicionar quantas quantidade do produto: ");
-                        p1.AddProduto(int.Parse(Console.ReadLine()));
-                        Console.WriteLine($"Dados Atualizados: {p1}");
-                        break;
-                    case 2:
-                        Console.Write("\nGostaria de remover quantos produtos: ");
-                        p1.DelProduto(int.Parse(Console.ReadLine()));
-                        Console.WriteLine($"Dados Atualizados: {p1}");
-                        break;
-                    case 3:
-                        resp = "Sim";
-                        break;
-                    default:
-                        Console.WriteLine("Opção invalida");
-                        break;
+                if(opcao == 3)
+                    resp = "Não";
+                else {
+                    switch(opcao)
+                    {
+                        case 1:
+                            Console.Write("\nGostaria de adicionar quantas quantidade do produto: ");
+                            p1.AddProduto(int.Parse(Console.ReadLine()));
+                            Console.WriteLine($"Dados Atualizados: {p1}");
+                            break;
+                        case 2:
+                            Console.Write("\nGostaria de remover quantos produtos: ");
+                            p1.DelProduto(int.Parse(Console.ReadLine()));
+                            Console.WriteLine($"Dados Atualizados: {p1}");
+                            break;
+                        default:
+                            Console.WriteLine("Opção invalida");
+                            break;
+                    }
+                    Console.Write("Você deseja fazer outra operação (Sim/Não): ");
+                    resp = Console.ReadLine();
                 }
-
-                Console.Write("Você deseja fazer outra operação (Sim/Não): ");
-                resp = Console.ReadLine();
             } while (resp == "Sim" || resp == "SIM" || resp == "sim" || resp == "s" || resp == "S");
             
 ;        }
