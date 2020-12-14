f5a16f2c67208fd14051f7cf4b48851eeecddce3
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-14 17:46:33 -0300

    creating looping for add product

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleEstoque
     {
         static void Main(string[] args)
         {
+            string resp;
             Produto p1 = new Produto("TV", 900.00, 10);
 
             Console.Clear();
@@ -16,9 +17,16 @@ namespace ConsoleEstoque
             p1.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.WriteLine($"Dados do Produto: {p1}");
             
-            Console.Write("\nGostaria de adicionar quantas quantidade do produto: ");
-            p1.AddProduto(int.Parse(Console.ReadLine()));
-            Console.WriteLine($"Dados Atualizados: {p1}");
+            do
+            {
+                Console.Write("\nGostaria de adicionar quantas quantidade do produto: ");
+                p1.AddProduto(int.Parse(Console.ReadLine()));
+                Console.WriteLine($"Dados Atualizados: {p1}");
+
+                Console.Write("Você deseja adicionar mais quantidades (Sim/Não): ");
+                resp = Console.ReadLine();
+            } while (resp == "Sim" || resp == "SIM" || resp == "sim" || resp == "s" || resp == "S");
+            
 ;        }
     }
 }
