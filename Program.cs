c475c68e6398fdd9dab4eecef7f8ebb9d44e7aa6
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-14 17:38:17 -0300

    creating product add functionality

--- Program.cs
+++ Program.cs
@@ -10,11 +10,15 @@ namespace ConsoleEstoque
             Produto p1 = new Produto("TV", 900.00, 10);
 
             Console.Clear();
-            Console.WriteLine("Digite o nome do produto: ");
+            Console.Write("Digite o nome do produto: ");
             p1.Nome = Console.ReadLine();
-            Console.WriteLine("Digite o preço inicial do produto: ");
+            Console.Write("Digite o preço inicial do produto: ");
             p1.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.WriteLine($"Dados do Produto: {p1}");
+            
+            Console.Write("\nGostaria de adicionar quantas quantidade do produto: ");
+            p1.AddProduto(int.Parse(Console.ReadLine()));
+            Console.WriteLine($"Dados Atualizados: {p1}");
 ;        }
     }
 }
