c91d75a58ab38c41aea97d785574da3afbe1b75f
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-14 16:36:29 -0300

    creating project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
